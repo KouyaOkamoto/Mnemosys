c96b3975f11086a36858233f44d320e33a6ba33c
KouyaOkamoto <oka.k.49132@gmail.com>
2025-06-02 08:18:35 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 8000,
-    host: true, // 外部からのアクセスを許可
-    open: true  // ブラウザを自動で開く
-  },
+  base: '/Mnemosys/',
   optimizeDeps: {
     exclude: ['@electric-sql/pglite']
   },
